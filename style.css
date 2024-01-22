784be6bb18d4c5a3f7d36829ea43dcab96f06173
primaverambrella <157367976+primaverambrella@users.noreply.github.com>
2024-01-22 14:26:20 +0300

    Add files via upload

---
+++ style.css
@@ -0,0 +1,15 @@
+function findGift() {
+    var fullName = document.getElementById("fullName").value;
+    var gift = getGiftByFullName(fullName);
+    document.getElementById("result").innerHTML = gift ? "Ваш подарок: " + gift : "Подарок для этого имени не найден.";
+}
+
+function getGiftByFullName(fullName) {
+    var gifts = {
+        "Реметников Александр Юрьевич": "бритва",
+        "Иванов Иван Иванович": "компьютер",
+        "Сидоров Сидор Сергеевич": "банка пива",
+        "Ключников Денис Фёдорович": "клюшка для хоккея"
+    };
+    return gifts[fullName];
+}
